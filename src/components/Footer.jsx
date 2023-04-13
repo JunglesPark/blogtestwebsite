97cec77cf03a39fca3cc8e98d7fc264685067397
Jung woo <jw.park@artzioneer.com>
2023-04-13 18:20:40 +0800

    Update Footer.jsx
    yes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,60 +2,25 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="w-full bg-[#02044A] text-gray-300 py-8 px-2">
-        <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600">
-
-            <div>
-                <h6 className="font-bold uppercase py-2">Solutions</h6>
-                <ol>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                </ol>
+    <div className="w-full h-[75vh] bg-[#02044A] text-gray-300">
+        <div className="relative h-[50%] bg-[url('https://picsum.photos/id/237/536/354')] bg-cover bg-no-repeat">
+            <div className="h-full w-[15%] bg-blue-500 skew-x-[-15deg] absolute bottom-10 left-10">
+            
             </div>
-
-            <div>
-                <h6 className="font-bold uppercase py-2">Solutions</h6>
-                <ol>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                </ol>
+            <div className="h-[60%] w-[50%] bg-white skew-x-[-15deg] absolute -bottom-10 right-32 opacity-75">
+                <div className="skew-x-[15deg]">
+                    <h2>Looking to fast track your way to success</h2>
+                    <button>Drop us a Message</button>
+                    <div className="grid grid-cols-3">
+                        <a href="/about">About Us</a>
+                        <a href="/services">Our Services</a>
+                        <a href="#">Contact Us</a>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div className="h-[50%] bg-cyan-100">
 
-            <div>
-                <h6 className="font-bold uppercase py-2">Solutions</h6>
-                <ol>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                </ol>
-            </div>
-
-            <div>
-                <h6 className="font-bold uppercase py-2">Solutions</h6>
-                <ol>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                    <li className="py-1">Marketing</li>
-                </ol>
-            </div>
-
-            <div className="col-span-2 pt-2 md:pt-2">
-                <p className="font-bold uppercase">Subscribe to our newsletter</p>
-                <p className="py-4">The latest news, articles and resources sent to your inbox weekly.</p>
-                <form className='flex flex-col sm:flex-row'>
-                    
-                </form>
-            </div>
         </div>
     </div>
   )
