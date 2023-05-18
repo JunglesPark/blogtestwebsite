2e04a0a87e0f12a69cebda0bb566f63b3bff1602
Jung woo <jw.park@artzioneer.com>
2023-05-18 15:39:04 +0800

    Changing Middleware ClientStory section
    Changing WIP

--- src/components/ClientStory.jsx
+++ src/components/ClientStory.jsx
@@ -5,35 +5,15 @@ import MiddlewareClientBG from "../assets/Middleware/backgroundMiddleware.png"
 const ClientStory = () => {
   return (
     <div className="relative flex bg-gray-200 justify-center w-screen h-full py-32 ">
-        <div className="grid grid-cols-12">
+
                    
-            <div className="bg-cover bg-center bg-no-repeat bg-[url('./assets/Middleware/backgroundMiddleware.png')] md:col-start-3 md:col-span-8 col-span-12">
-                
+            <div className="bg-cover bg-center bg-no-repeat bg-blue-500 w-[80vw] md:col-start-3 md:col-span-8 col-span-12">
                 <div className="relative md:w-[135px] md:h-[135px] w-[100px] h-[100px] md:translate-x-[-50%] translate-x-[0%] md:translate-y-[-50%] translate-y-[-75%] bg-cover bg-[url('../src/assets/Middleware/HKJCLogo.png')] z-50">
                 </div>
                 <div className="relative z-40 pl-12 md:translate-x-[0%] md:translate-y-[-200%] translate-y-[-162%] lg:w-[500px] w-[400px] bg-cyan-500" style={{clipPath: 'polygon(0% 0%, 80% 0%, 100% 50%, 80% 100%, 0% 100%)'}}>
                     <h2 className="p-2 md:pl-5 pl-14 text-white text-xl font-bold lg:tracking-widest">Story of Client</h2>
                     <hr className=""></hr>
                     <h3 className="p-2 pl-5 text-white text-xl font-black lg:tracking-widest">Hong Kong Jockey Club</h3>
-                </div>
-                
-                <div className="relative translate-y-[0px] md:translate-y-[-50px]">
-                    <div className="px-10 w-100%">
-                        <p className='text-lg font-semibold text-white'>Our middleware professionals can engineer a platform for your data systems to operate in a scalable and fault tolerant manner.</p>
-                    </div>
-                    <div className="border-4 border-inset p-8 mx-8 my-20 flex bg-white md:h-[360px] h-[400px]">
-                        <div className="px-10 lg:w-2/3 ">
-                            <div>
-                                <h1 className="px-6 py-2 w-fit xl:text-3xl text-xl text-cyan-500 font-bold border-2 border-solid rounded-full border-cyan-200">Data Integration</h1>
-                            </div>
-                            <p className="py-5 lg:text-base text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                        </div>
-                        <div className="lg:w-1/3 w-0 self-center">
-                            <img className="invisible lg:visible w-full drop-shadow-sm" src="../src/assets/Middleware/Hero4_photo_1.png" />
-                        </div>
-                    </div>
-
-                   
                 </div>
             </div>
             {/* Main info section */}
@@ -94,7 +74,7 @@ const ClientStory = () => {
             </div>
          */}
         
-        </div>
+        
     </div>
   )
 }
