0857843aee0fc25ca6ca48812cde927649a6bc6d
Jung woo <jw.park@artzioneer.com>
2023-05-19 12:42:33 +0800

    a
    a

--- src/components/ClientStory.jsx
+++ src/components/ClientStory.jsx
@@ -4,18 +4,24 @@ import MiddlewareClientBG from "../assets/Middleware/backgroundMiddleware.png"
 
 const ClientStory = () => {
   return (
-    <div className="relative flex bg-gray-200 justify-center w-screen h-full py-32 ">
+    <div className="relative flex bg-gray-100 justify-center w-screen h-full py-32 ">
 
                    
-            <div className="bg-cover bg-center bg-no-repeat bg-blue-500 w-[80vw] md:col-start-3 md:col-span-8 col-span-12">
-                <div className="relative md:w-[135px] md:h-[135px] w-[100px] h-[100px] md:translate-x-[-50%] translate-x-[0%] md:translate-y-[-50%] translate-y-[-75%] bg-cover bg-[url('../src/assets/Middleware/HKJCLogo.png')] z-50">
+            <div className="ClientStoryBackground w-[80vw] md:col-start-3 md:col-span-8 col-span-12">
+                <div>
+                    <div className="relative md:w-[135px] md:h-[135px] w-[100px] h-[100px] md:translate-x-[-50%] translate-x-[0%] md:translate-y-[-50%] translate-y-[-75%] bg-cover bg-[url('../src/assets/Middleware/HKJCLogo.png')] z-50">
+                    </div>
+                    <div className="relative z-40 pl-12 md:translate-x-[0%] md:translate-y-[-200%] translate-y-[-162%] lg:w-[500px] w-[400px] bg-cyan-500" style={{clipPath: 'polygon(0% 0%, 80% 0%, 100% 50%, 80% 100%, 0% 100%)'}}>
+                        <h2 className="p-2 md:pl-5 pl-14 text-white text-xl font-bold lg:tracking-widest">Story of Client</h2>
+                        <hr className=""></hr>
+                        <h3 className="p-2 pl-5 text-white text-xl font-black lg:tracking-widest">Hong Kong Jockey Club</h3>
+                    </div>
                 </div>
-                <div className="relative z-40 pl-12 md:translate-x-[0%] md:translate-y-[-200%] translate-y-[-162%] lg:w-[500px] w-[400px] bg-cyan-500" style={{clipPath: 'polygon(0% 0%, 80% 0%, 100% 50%, 80% 100%, 0% 100%)'}}>
-                    <h2 className="p-2 md:pl-5 pl-14 text-white text-xl font-bold lg:tracking-widest">Story of Client</h2>
-                    <hr className=""></hr>
-                    <h3 className="p-2 pl-5 text-white text-xl font-black lg:tracking-widest">Hong Kong Jockey Club</h3>
-                </div>
+
+               
             </div>
+
+            
             {/* Main info section */}
 
             {/* 
