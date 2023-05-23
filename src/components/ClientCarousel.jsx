316c6d4b995fb4f07e15eac43f446b1a321446c5
chungw <chung94118@gmail.com>
2023-05-23 17:03:42 +0800

    Update UI
    - update client and partner UI

--- src/components/ClientCarousel.jsx
+++ src/components/ClientCarousel.jsx
@@ -1,65 +1,108 @@
-import React, {useState} from 'react'
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import React, {useContext, useState, useEffect} from 'react'
+import { motion } from "framer-motion"
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import 'react-horizontal-scrolling-menu/dist/styles.css';
+import { LeftArrow,RightArrow, HeaderArrows } from './ScrollingMenu/Arrows';
+import usePreventBodyScroll from './ScrollingMenu/usePreventBodyScroll'
+// NOTE: for hide scrollbar
+import "./ScrollingMenu/hideScrollbar.css";
 
-const ClientCarousel = () => {
-    const [activePhoto, setActivePhoto] = useState(0)
+import { screenSize } from '../hooks/screenSize'
 
-    const scrollLeft = (currentIndex) => {
-        let scrollIndex = "item_" + (activePhoto > 0 ? activePhoto-1 : activePhoto).toString()
-        console.log(scrollIndex)
-        const scrollToItem = document.getElementById(scrollIndex)
-        scrollToItem?.current.scrollIntoView()
-        setActivePhoto(activePhoto > 0 ? activePhoto-1 : activePhoto)
+
+const elemPrefix = "item-"
+const getId = (index) => `${elemPrefix}${index}`
+
+
+const ClientCarousel = ({assignActiveItem , clientList}) => {
+    let screenSizeWidth = screenSize()
+    const getItems = () => clientList.map((_,ind)=>({id: getId(ind)}));
+    const [items,  setItems] = useState(getItems)
+    const [position, setPosition] = useState('item-0')
+    const [selected, setSelected] = useState([])
+    const [photoContainer, setPhotoContainer] = useState(0)
+
+
+    const isItemSelected = (id) => !!selected.find((el)=> el === id);
+
+    useEffect(()=>{
+        let photoContainerWidth = Math.floor(screenSizeWidth * 0.8)
+        console.log(photoContainerWidth)
+        setPhotoContainer(photoContainerWidth)
+    },[screenSizeWidth])
+
+    const handleContainerWidth = () =>{
+        let photoContainerCss = "relative h-[450px] p-5 xs:max-w-[325px] w-[325px] md:w-[550px] lg:w-[650px] xl:w-[750px] 2xl:w-[850px]"
+        return photoContainerCss
     }
-    const scrollRight = (currentIndex) => {
-        let newIndex = currentIndex + 1
-        let scrollElement = "item_" + newIndex
-        const scrollToItem = document.getElementById(scrollElement)
-        scrollToItem.scrollIntoView({behavior:'smooth', block:'nearest', inline:'start'});
-        // document.getElementById("content").scrollLeft += 400;
+
+    const handleClick = (id) => ({getItemById, scrollToItem}) => {
+        const itemSelected = isItemSelected(id)
+        
+        setSelected((currentSelected) => 
+            itemSelected ? 
+            currentSelected.filter((el)=>el !== id):
+            currentSelected.concat(id)
+        )
+
+        assignActiveItem(id)
+        setPosition(id)
     }
 
-    const PhotoContainer = ({id}) =>{
+    const ArrowHeaderContainer = () =>{
         return(
-            <div id={id}>
-                <div className="card skew-x-[10deg] bg-cyan-100 w-[200px] h-[350px] m-2 rounded-lg shadow-lg">
+            <>
+            <div className='flex justify-start align-middle'>
+                <LeftArrow/><RightArrow/>
+            </div>
+            </>
+        )
+    }
+    
+
+    const PhotoCardContainer = ({onClick, selected, title, itemId}) =>{
+        const visibility = useContext(VisibilityContext)
+        const visible = visibility.isItemVisible(itemId)
+        const itemIndex = itemId.split('item-')[1]
+
+        const cardContainerCss = (visible ,itemId) => {
+            var style = "card w-[200px] h-[350px] m-2 rounded-lg shadow-lg cursor-pointer"
+            if (!visible){
+                style += " opacity-70 bg-white"
+            } else {
+                style += " bg-[#00498E]/[.08]"
+            }
+
+            if(itemId === position){
+                style += " border border-[#239CFF]"
+            }
+            return style
+        }
+
+        return(
+            <div onClick={()=>onClick(visibility)} tabIndex={0} >
+                <motion.div whileHover={{scale:1.02, transition:{ duration: 0.5}}} className={cardContainerCss(visible, itemId)}>
                     <div className="flex place-content-center place-items-center h-full w-full">
                         <img
-                            className="skew-x-[-10deg] w-[100%] h-[50%] p-2"
-                            src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204"
+                            className="w-[90%] h-[50%] p-2"
+                            src={clientList[itemIndex].image}
                             alt="img"
                         />
                     </div>
-                </div>
+                </motion.div>
             </div>
         )
     }
 
-    const Items = () =>{
-        var photos = []
-        for (let i = 0 ; i<7 ; i++){
-            photos.push(<PhotoContainer key={i} id={'item_' + i}/> )
-        }
-        return(
-            <>
-                {photos}
-            </>
-        )
-    }
 
   return (
-    <div className="relative">
-        <div>
-            <button onClick={scrollLeft} className="p-2 m-2 rounded-full bg-white">
-            <FiChevronLeft />
-            </button>
-            <button onClick={()=>scrollRight(activePhoto)} className="p-2 m-2 rounded-full bg-white">
-            <FiChevronRight />
-            </button>
-        </div>
-        <div id="content" className="carousel p-4 flex items-center justify-start overflow-x-scroll px-12 scroll-smooth  scrollbar-hide">
-            <Items/>
-        </div>
+    <div className={handleContainerWidth()}>
+        <ScrollMenu Header={ArrowHeaderContainer} options={{ratio:0.9, rootMargin:"5px", threshold:[0.01,0.05,0.5,0.75,0.95,1]}}>
+            {items.map(({id})=>(
+                <PhotoCardContainer itemId={id} title={id} key={id} selected={isItemSelected(id)} onClick={handleClick(id)}/>
+            ))}
+        </ScrollMenu>
+    
     </div>
   )
 }
